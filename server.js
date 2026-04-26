837fa4a5676b438e9d355453e05a5d3c416368d1
VEDANT TAPASE <vedanttapase70@gmail.com>
2026-04-27 02:36:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -123,9 +123,9 @@ app.delete('/api/items/:id', (req, res) => {
 
 // --- SERVER START ---
 // Render provides a port via process.env.PORT
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
+
 app.listen(PORT, '0.0.0.0', () => {
-    console.log('-----------------------------------');
-    console.log(`Server running on port ${PORT}`);
-    console.log('-----------------------------------');
+    console.log(`Server is running on port ${PORT}`);
+});
 });
